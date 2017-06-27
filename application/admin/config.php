13974aba07db1f4cf77dc2657cacbc093d6dad6a
元炳烨 <nikori1990@gmail.com>
2017-06-28 00:34:42 +0800

    admin config

--- application/admin/config.php
+++ application/admin/config.php
@@ -1,5 +1,15 @@
 <?php
 //配置文件
 return [
+    'view_replace_str'       => [
+        '__JS__'         =>  '/static/admin/js'             ,
+        '__CSS__'        =>  '/static/admin/css'            ,
+        '__IMG__'        =>  '/static/admin/img'            ,
 
+        '__JQUERY__'                =>  '/static/common/jquery/dist'                ,
+        '__AMAZEUI__'               =>  '/static/common/amazeui/dist'               ,
+        '__AMAZEUI-DATATABLES__'    =>  '/static/common/amazeui-datatables/dist'    ,
+        '__BOOTSTRAP__'             =>  '/static/common/bootstrap/dist'             ,
+        '__ECHARTS__'               =>  '/static/common/echarts/dist'               ,
+    ],
 ];
